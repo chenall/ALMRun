48cea4814f95814e65fc9a5d332229dd954e70c9
chenall <chenall.cn@gmail.com>
2013-11-16 15:42:47 +0800

    为了方便以后对LUA模块进行扩展，分离LUA的DLL库，改为外置。

--- src/ALMRunVersion.h
+++ src/ALMRunVersion.h
@@ -1,6 +1,6 @@
 #ifndef _ALMRUN_VERSION_H_
 #define _ALMRUN_VERSION_H_
-#define VERSION_INT  1,2,0,34
-#define VERSION_STR  "1,2,0,34"
-#define VERSION_DATE  "2013/11/14"
+#define VERSION_INT  1,2,0,35
+#define VERSION_STR  "1,2,0,35"
+#define VERSION_DATE  "2013/11/16"
 #endif
