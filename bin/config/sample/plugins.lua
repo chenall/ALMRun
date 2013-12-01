5c27c01b0b43b303fc97f3878e6eea110e32ca77
chenall <chenall.cn@gmail.com>
2013-12-01 09:00:46 +0800

    示范插件命令作了一些调整

--- bin/config/sample/plugins.lua
+++ bin/config/sample/plugins.lua
@@ -1,11 +1,16 @@
 RegisterPlugin('command',{name='LUA',func = function(commandName,...)
-    return {name="执行LUA命令:"..commandName,desc="ALMRun 插件示范",cmd = function(arg)
+    local t_command = {name="执行LUA命令:"..commandName,desc="ALMRun 插件示范",order=1000}
+    if commandName:len() > 1 then
+	t_command.cmd = function(arg)
 	    MessageBox(loadstring("return "..commandName)(arg))
-	end}
+	end
+    end
+    return t_command
 end})
 
+--order值为负数,使得这个命令的优先级比较低,也就是保持这个命令在列表尾部
 RegisterPlugin('command',{func=function(commandName,...)
-    return {name="Run "..commandName,desc="ALMRun 运行插件",cmd=commandName}
+    return {name="Run "..commandName,desc="ALMRun 运行插件",cmd = commandName,order=-1}
 end})
 
 --计算器插件示范，返回多个命令，通过order值对命令进行排序，值高的排前面
