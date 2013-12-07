96bd2d0e3c81323a6d650c513610873fcc8fcf60
chenall <chenall.cn@gmail.com>
2013-12-07 10:03:38 +0800

    v1.2.0.51

--- src/ALMRunVersion.h
+++ src/ALMRunVersion.h
@@ -1,6 +1,6 @@
 #ifndef _ALMRUN_VERSION_H_
 #define _ALMRUN_VERSION_H_
-#define VERSION_INT  1,2,0,51
-#define VERSION_STR  "1,2,0,51"
-#define VERSION_DATE  "2013/12/06"
+#define VERSION_INT  1,2,0,52
+#define VERSION_STR  "1,2,0,52"
+#define VERSION_DATE  "2013/12/07"
 #endif
