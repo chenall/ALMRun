cb39523bd4cbbf03454992eedd6f02a6d710291b
chenall <chenall.cn@gmail.com>
2014-11-01 15:45:01 +0800

    v1.2.0.70

--- src/ALMRunVersion.h
+++ src/ALMRunVersion.h
@@ -1,6 +1,6 @@
 #ifndef _ALMRUN_VERSION_H_
 #define _ALMRUN_VERSION_H_
-#define VERSION_INT  1,2,0,69
-#define VERSION_STR  "1,2,0,69"
-#define VERSION_DATE  "2014/09/24"
+#define VERSION_INT  1,2,0,70
+#define VERSION_STR  "1,2,0,70"
+#define VERSION_DATE  "2014/11/01"
 #endif
