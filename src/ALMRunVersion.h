ce678c8734c2d04514777a555bd584f06bc1c17a
chenall <chenall.cn@gmail.com>
2015-01-26 11:05:43 +0800

    v1.2.0.73

--- src/ALMRunVersion.h
+++ src/ALMRunVersion.h
@@ -1,6 +1,6 @@
 #ifndef _ALMRUN_VERSION_H_
 #define _ALMRUN_VERSION_H_
-#define VERSION_INT  1,2,0,72
-#define VERSION_STR  "1,2,0,72"
-#define VERSION_DATE  "2014/12/21"
+#define VERSION_INT  1,2,0,73
+#define VERSION_STR  "1,2,0,73"
+#define VERSION_DATE  "2015/01/26"
 #endif
