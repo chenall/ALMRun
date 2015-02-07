6d458f3d9d0c897d3fb7ace7c263b483712936c7
chenall <chenall.cn@gmail.com>
2015-02-07 17:16:52 +0800

    wxWidget path changed

--- src/MyTextCompleter.h
+++ src/MyTextCompleter.h
@@ -3,7 +3,16 @@
 
 #include "MerryWx.h"
 #include <wx/textcompleter.h>
+/*
+为了实现任意位置匹配,需要修改wxWidget源码
+src\msw\textentry.cpp
+查找 pAutoComplete2->SetOptions
+添加 ACO_NOPREFIXFILTERING 实现任意位置匹配
 
+另一个改动上面同一个文件中
+查找 wxBIND_OR_CONNECT_HACK(m_win, wxEVT_AFTER_CHAR,
+修改为 wxBIND_OR_CONNECT_HACK(m_win, wxEVT_CHAR,
+*/
 class MyTextCompleter : public wxTextCompleter
 {
 public:
