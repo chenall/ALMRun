133b65683f7ab7864bc889c2031f4ae0250d23d3
chenall <chenall.cn@gmail.com>
2016-03-09 13:45:57 +0800

    版本号改为1.2.0.74

--- src/ALMRunVersion.h
+++ src/ALMRunVersion.h
@@ -1,6 +1,6 @@
 #ifndef _ALMRUN_VERSION_H_
 #define _ALMRUN_VERSION_H_
-#define VERSION_INT  1,2,0,73
-#define VERSION_STR  "1,2,0,73"
-#define VERSION_DATE  "2015/01/26"
+#define VERSION_INT  1,2,0,74
+#define VERSION_STR  "1,2,0,74"
+#define VERSION_DATE  "2016/03/09"
 #endif
